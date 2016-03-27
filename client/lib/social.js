eafe37cbf03501f546813d3ca2493deb4c652fdd
Tan Quang Ngo <quangngotan95@gmail.com>
2016-03-28 02:39:36 +0800

    Changes log: - Add publish method for twitter - Change routing: default is 'public', require 'login' for operators' actions - Add login logic

--- client/lib/social.js
+++ client/lib/social.js
@@ -2,34 +2,16 @@
  * Created by QuangNgo on 22/3/16.
  */
 
-var pageId = '1675747249379266';
-var pageAccessToken;
+var pageId = '1675747249379266';    // facebook page id
+var pageAccessToken; // access token for Facebook page
 
-function generateFacebookPost() {
-    var type = $('#incident').val();
-    var content = $('#content').val();
-    var publishMessage = 'Incident type ' + type + ' happened!\n'
-        + 'Details: ' + content;
-    return publishMessage;
-}
-
-function publishToFacebook(publishMessage) {
-    FB.api('/me/accounts/', function(response) {
-        for (var i = 0; i<response.data.length; i++) {
-            if (response.data[i].id === pageId) {
-                pageAccessToken = response.data[i].access_token;
-                break;
-            }
-        }
-        console.log(pageAccessToken);
-        FB.api('/me/feed/', 'post', {
-            access_token: pageAccessToken,
-            message: publishMessage
-        }, function(response) {
-            console.log(response);
-        })
-    });
-}
+/**
+ * Twitter app constants
+ */
+var T_consumer_key = 'QiqUFJ5YJHEJoVxNxb1TerbZn';
+var T_consumer_secret = 'qYm31q3zPqey5Ok12Z4PkWjhesnUR8UOoXNKbT6UiTvecfcrjF';
+var T_access_token = '712817952941170688-PYuSdJI2NDC3y5kHrM8kZMuiwwZdP32';
+var T_access_token_secret = '5IsqmZzrLXpgHPIek0wKIjO9SHvDuRp03sbxaAxODVWqG';
 
 if (Meteor.isClient) {
     window.fbAsyncInit = function() {
@@ -40,26 +22,62 @@ if (Meteor.isClient) {
         });
     };
 
-
-
     Template.social.events({
         'click #publishBtn': function (event) {
             event.preventDefault();
-            var publishMessage = generateFacebookPost();
-            FB.getLoginStatus(function(response) {
-                if (response.status === 'connected') {
-                    console.log('Logged in.');
-                    publishToFacebook(publishMessage);
-                }
-                else {
-                    FB.login(function(){
-                        // Note: The call will only work if you accept the permission request
-                    publishToFacebook(publishMessage);
-                    }, {scope: 'publish_actions'});
-                }
-            });
+            //publishToFacebook();
+            publishToTwitter();
         }
     });
 
+}
+
+function generatePost() {
+    var type = $('#incident').val();
+    var content = $('#content').val();
+    return 'Incident type ' + type + ' happened!\n' + 'Details: ' + content;
+}
+
+function postToPage(publishMessage) {
+    FB.api('/me/accounts/', function(response) {
+        for (var i = 0; i<response.data.length; i++) {
+            if (response.data[i].id === pageId) {
+                pageAccessToken = response.data[i].access_token;
+                break;
+            }
+        }
+        console.log(pageAccessToken);
+        FB.api('/me/feed/', 'POST', {
+            access_token: pageAccessToken,
+            message: publishMessage
+        }, function(response) {
+            console.log(response);
+        })
+    });
+}
+
+function publishToFacebook() {
+    var publishMessage = generatePost();
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            console.log('Logged in.');
+            postToPage(publishMessage);
+        }
+        else {
+            FB.login(function(){
+                // Note: The call will only work if you accept the permission request
+                postToPage(publishMessage);
+            }, {scope: 'publish_actions'});
+        }
+    });
+}
+
+function initTwitter() {
+}
+
+function publishToTwitter() {
+    var publishMessage = generatePost();
+    var twtLink = 'http://twitter.com/home?status=' +encodeURIComponent(publishMessage);
+    window.open(twtLink,'_blank');
 
 }
